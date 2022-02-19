c3fe0e5c3fbe3abcf860dcfe4ce43e70023a4fad
Coal <dibashtori@gmail.com>
2022-02-18 21:44:52 -0600

    Switch to toml11 for TOML parsing

--- lpm/macros.h
+++ lpm/macros.h
@@ -5,10 +5,30 @@
     #define LPM_SHOULD_PRINT_ERRORS 1
 #endif
 
-#if LPM_SHOULD_PRINT_ERRORS
-    #define LPM_PRINT_ERROR(msg) std::cerr << msg << std::endl
-#else
-    #define LPM_PRINT_ERROR(msg)
+#ifndef LPM_DEBUG_MODE
+    #define LPM_DEBUG_MODE 1
 #endif
 
-#define LPM_PRINT(msg) std::cout << msg << std::endl
+#ifndef LPM_SILENT
+    #define LPM_SILENT 0
+#endif
+
+#if !LPM_SILENT
+    #if LPM_SHOULD_PRINT_ERRORS
+        #define LPM_PRINT_ERROR(msg) std::cerr << msg << std::endl
+    #else
+        #define LPM_PRINT_ERROR(msg)
+    #endif
+
+    #if LPM_DEBUG_MODE
+        #define LPM_PRINT_DEBUG(msg) std::cerr << msg << std::endl
+    #else
+        #define LPM_PRINT_DEBUG(msg)
+    #endif
+
+    #define LPM_PRINT(msg) std::cout << msg << std::endl
+#else
+    #define LPM_PRINT_ERROR(msg)
+    #define LPM_PRINT_DEBUG(msg)
+    #define LPM_PRINT(msg)
+#endif
--- lpm/packages.cpp
+++ lpm/packages.cpp
@@ -3,6 +3,46 @@
 #include <string_view>
 #include "packages.h"
 #include "macros.h"
+#include "toml11/toml.hpp"
 
 void LPM::PackagesData::load() {
+    toml::value data;
+    try {
+        data = toml::parse(this->path);
+    } catch (...) {
+        LPM_PRINT_ERROR("Failed to parse: " << this->path);
+    }
+
+    this->name = toml::find_or(data, "project.name", "");
+    this->version = toml::find_or(data, "project.version", "");
+    this->description = toml::find_or(data, "project.description", "");
+    this->author = toml::find_or(data, "project.author", "");
+    this->license = toml::find_or(data, "project.license", "");
+    this->homepage = toml::find_or(data, "project.homepage", "");
+    this->repository = toml::find_or(data, "project.repository", "");
+    this->main = toml::find_or(data, "project.main", "");
+    this->lua_version = toml::find_or(data, "project.lua_version", "");
+
+    if (data.contains("dependencies")) {
+        this->dependencies = toml::find<
+            std::vector<std::string>
+        >(data, "dependencies");
+    }
+
+    LPM_PRINT("Loaded data for project: " << this->name);
+
+    // print all of the parsed data
+    LPM_PRINT("this->name : " << this->name);
+    LPM_PRINT("this->version : " << this->version);
+    LPM_PRINT("this->description : " << this->description);
+    LPM_PRINT("this->author : " << this->author);
+    LPM_PRINT("this->license : " << this->license);
+    LPM_PRINT("this->homepage : " << this->homepage);
+    LPM_PRINT("this->repository : " << this->repository);
+    LPM_PRINT("this->main : " << this->main);
+    LPM_PRINT("this->lua_version : " << this->lua_version);
+    LPM_PRINT("this->dependencies.size() : " << this->dependencies.size());
+    for (auto& dependency : this->dependencies) {
+        LPM_PRINT("this->dependencies : " << dependency);
+    }
 }
